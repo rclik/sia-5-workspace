e7e7a02f22a64ec57c76ddb47fd3f4332c1548f4
rcelik <rahmancelik@gmail.com>
2020-03-16 00:16:13 +0300

    spring boot ve spring nedir, ne ise yarar. basit olarak spring boot application i yaratmak, sonrasinda project structure i uzerinde calisildi.

---
+++ chapter-one/src/test/java/com/rcelik/siafive/chapterone/ChapterOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcelik.siafive.chapterone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapterOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
